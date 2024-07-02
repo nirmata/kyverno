416684ed6afa35e4363570d16988332fddfd875f
Anushka Mittal <anushka@nirmata.com>
2024-07-02 22:12:45 +0530

    NDEV-19576 : invalid mutation fix
    Signed-off-by: Anushka Mittal <anushka@nirmata.com>

--- pkg/engine/handlers/mutation/common.go
+++ pkg/engine/handlers/mutation/common.go
@@ -13,7 +13,7 @@ import (
 	"github.com/kyverno/kyverno/pkg/engine/mutate/patch"
 	engineutils "github.com/kyverno/kyverno/pkg/engine/utils"
 	"github.com/kyverno/kyverno/pkg/utils/api"
-	"github.com/mattbaird/jsonpatch"
+	"gomodules.xyz/jsonpatch/v2"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
